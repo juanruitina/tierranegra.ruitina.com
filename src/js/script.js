23971c95cd556ec4371f4a48e955018375af1a49
Juan Ruitiña <git@ruitina.com>
2021-06-13 11:37:13 +0200

    Add Mallada note
    CSS smooth scroll
    Remove commented styles and scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,9 @@
 /* Animaciones */
 $(document).ready(function(){
-   //$("#top-nav,#caja,#texto,#autor,#manana-texto").hide();
-
-   //$(".inicio .banner,.lucha .banner,.triunfo .banner").delay(1500).queue(function(next) {$(this).addClass("bn-dark");next();});
-
    $("#caja").delay(500).fadeIn(2000);
    $("#texto").delay(1500).fadeIn(1500);
    $("#top-nav").delay(3000).slideDown(500);
    $("#autor").delay(3000).fadeIn(500);
-
-   /*$("#intro-caja,#intro-texto").hide();
-   $("#intro-caja").delay(1500).fadeIn(2000);
-   $("#intro-texto").delay(2000).fadeIn(1500);*/
 
    $("#manana-texto").delay(500).fadeIn(1000);
 
@@ -22,6 +14,7 @@ $(document).ready(function(){
       $(this).toggleClass('open');
    });
 });
+
 if ($(window).width() >= 800) {
    $(window).scroll(function() {
        var scroll = $(window).scrollTop();
@@ -30,15 +23,6 @@ if ($(window).width() >= 800) {
        $('.manana .banner').css({'-webkit-filter': 'grayscale(' + ( 100-scroll/scrollAnimationSpeed )/100}) + '%)';
    });
 }
-/* Fix alto de pantalla para móviles
-if ($(window).width() < 800) {
-   $(document).ready(function(){
-      $('.lucha header,.triunfo header,.manana header .content-a').css({'height': $(window).height()});
-      $('.inicio header').css({'height': $(window).height() * 1.2 });
-      $('.manana header').css({'height': $(window).height() * 1.5 });
-      $('.siguiente').css({'height': $(window).height() * 0.6 });
-   });
-}*/
 
 /* RESIZE */
 $(window).resize(function(){
@@ -47,25 +31,6 @@ $(window).resize(function(){
         $(this).height($(this).width());
     });
 }).resize();
-
-/* smooth scroll (eg title) */
-$(document).ready(function($) {
-    $(".smooth").click(function(event){
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
-    });
-});
-
-$("a[href^=#]").on("click", function(e) {
-    e.preventDefault();
-    history.pushState("", document.title, window.location.pathname);
-});
-
-/* Botón grande https://css-tricks.com/snippets/jquery/make-entire-div-clickable/ */
-/*$(".siguiente a").each(function() {
-    var href = this.href;
-    $(".siguiente").addClass("siguiente-hover").click(function() { location.href = href; });
-});*/
 
 $(".siguiente, .button-link").hover(
    function() {
